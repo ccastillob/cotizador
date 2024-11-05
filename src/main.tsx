2e90f7de76b7e3bed450ed819d6fddada35801e3
Christofer Castillo <ccastillo993a@gmail.com>
2024-11-05 15:41:37 -0500

    feat: implement provider to component main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Landing } from "./Landing";
 import "./scss/styles.scss";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Landing />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <Landing />
+    </StrictMode>
+  </Provider>
 );
