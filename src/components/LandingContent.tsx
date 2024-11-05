1e833dad946e4f9c896f6a759a5403794da0376e
Christofer Castillo <ccastillo993a@gmail.com>
2024-11-05 15:48:19 -0500

    feat: create LandingContent component

---
+++ src/components/LandingContent.tsx
@@ -0,0 +1,12 @@
+export const LandingContent = () => {
+  return (
+    <section className="landing-content">
+      <h3 className="landing-content__title">
+        El mejor <br /> tipo de cambio
+      </h3>
+      <p className="landing-content__description">
+        para cambiar dólares y soles <br /> online en Perú
+      </p>
+    </section>
+  );
+};
