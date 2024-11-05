abb7c4728ded68776619881454fcf0a0c622ffa8
Christofer Castillo <ccastillo993a@gmail.com>
2024-11-05 02:33:36 -0500

    feat: add component Landing

---
+++ src/Landing.tsx
@@ -0,0 +1,43 @@
+export const Landing = () => {
+  return (
+    <main className="landing">
+      <section>
+        <article>
+          <h3>
+            El mejor <br /> tipo de cambio
+          </h3>
+          <p>para cambiar dólares y soles online en Perú</p>
+        </article>
+        <article></article>
+      </section>
+    </main>
+  );
+};
+
+/**
+ <section className="hero-text">
+        <h3 className="hero-text__title">El mejor tipo de cambio</h3>
+        <p className="hero-text__description">
+          para cambiar dólares y soles <br /> online en Perú
+        </p>
+      </section>
+
+      <section className="hero-app">
+        <div className="hero-app__tabs">
+          <button className="hero-app__tab">
+            <div className={`hero-app__tab-content`}>
+              <span>Dólar compra</span>
+              <span className="hero-app__tab-content-number"></span>
+            </div>
+          </button>
+          <button className="hero-app__tab">
+            <div className={`hero-app__tab-content`}>
+              <span>Dólar venta</span>
+              <span className="hero-app__tab-content-number"></span>
+            </div>
+          </button>
+        </div>
+
+        <form className="hero-app__form"></form>
+      </section>
+ */
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Landing } from "./Landing";
+import "./scss/styles.scss";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Landing />
+  </StrictMode>
+);
