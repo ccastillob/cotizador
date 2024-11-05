5a9655a77a294e565c43c7caffb9b8f143220dd8
Christofer Castillo <ccastillo993a@gmail.com>
2024-11-05 15:49:29 -0500

    feat: implment components LandingContent and LandingCard

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -1,43 +1,11 @@
+import { LandingContent } from "./components/LandingContent";
+import { LandingCard } from "./components/LandingCard";
+
 export const Landing = () => {
   return (
-    <main className="landing">
-      <section>
-        <article>
-          <h3>
-            El mejor <br /> tipo de cambio
-          </h3>
-          <p>para cambiar dólares y soles online en Perú</p>
-        </article>
-        <article></article>
-      </section>
+    <main className="main-landing">
+      <LandingContent />
+      <LandingCard />
     </main>
   );
 };
-
-/**
- <section className="hero-text">
-        <h3 className="hero-text__title">El mejor tipo de cambio</h3>
-        <p className="hero-text__description">
-          para cambiar dólares y soles <br /> online en Perú
-        </p>
-      </section>
-
-      <section className="hero-app">
-        <div className="hero-app__tabs">
-          <button className="hero-app__tab">
-            <div className={`hero-app__tab-content`}>
-              <span>Dólar compra</span>
-              <span className="hero-app__tab-content-number"></span>
-            </div>
-          </button>
-          <button className="hero-app__tab">
-            <div className={`hero-app__tab-content`}>
-              <span>Dólar venta</span>
-              <span className="hero-app__tab-content-number"></span>
-            </div>
-          </button>
-        </div>
-
-        <form className="hero-app__form"></form>
-      </section>
- */
