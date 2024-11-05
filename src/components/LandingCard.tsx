b1a0200192fd10892664d43d7c0ebecde7c0fd2e
Christofer Castillo <ccastillo993a@gmail.com>
2024-11-05 15:48:55 -0500

    feat: implement components ContainerTab and ContainerForm

---
+++ src/components/LandingCard.tsx
@@ -0,0 +1,11 @@
+import { ContainerTab } from "./ContainerTab";
+import { ContainerForm } from "./ContainerForm";
+
+export const LandingCard = () => {
+  return (
+    <section className="landing-card">
+      <ContainerTab />
+      <ContainerForm />
+    </section>
+  );
+};
