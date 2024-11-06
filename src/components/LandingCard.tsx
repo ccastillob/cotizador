4f712cd212e5fb48ae9839cbc442c9a38d9a2870
Christofer Castillo <ccastillo993a@gmail.com>
2024-11-06 11:17:10 -0500

    feat: moving logic to LandingCard

--- src/components/LandingCard.tsx
+++ src/components/LandingCard.tsx
@@ -1,7 +1,38 @@
 import { ContainerTab } from "./ContainerTab";
 import { ContainerForm } from "./ContainerForm";
+import { doc, onSnapshot } from "firebase/firestore";
+import { dbFirebase } from "../firebase/config";
+import { useEffect } from "react";
+import { useRate } from "../hooks";
 
 export const LandingCard = () => {
+  const { updatePurchasePrice, updateSalePrice } = useRate();
+  const docRef = doc(dbFirebase, "rates", "awaOMswZ8JGxjmHCpVZ4");
+
+  useEffect(() => {
+    const unsub = onSnapshot(
+      docRef,
+      (doc) => {
+        if (!doc.exists()) return;
+
+        const { purchase_price, sale_price } = doc.data();
+        updatePurchasePrice(purchase_price);
+        updateSalePrice(sale_price);
+      },
+      (error) => {
+        console.log(error);
+        // Al parecer me faltan permisos para que me devuelva una respuesta, por ello decidí colocarle unos valores por defecto,
+        // de esta manera el app sigue funcionando hasta que la API pueda funcionar
+        updatePurchasePrice(3.924);
+        updateSalePrice(3.945);
+      }
+    );
+
+    return () => {
+      unsub();
+    };
+  }, []);
+
   return (
     <section className="landing-card">
       <ContainerTab />
